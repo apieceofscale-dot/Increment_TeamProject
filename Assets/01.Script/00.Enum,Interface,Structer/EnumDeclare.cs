bfa020b19d26b63b6b735c792d28a6b4a5d08715
FlynnSohn <137607802+FlynnSohn@users.noreply.github.com>
2026-08-31 18:44:27 +0900

    fix: 인터페이스 의존성 주입/초기화 분리

--- Assets/01.Script/00.Enum,Interface,Structer/EnumDeclare.cs
+++ Assets/01.Script/00.Enum,Interface,Structer/EnumDeclare.cs
@@ -1,5 +1,4 @@
-
-
+// 아까 enum값 중복되지 않게 하라 말씀주셨는데 일단 폴더 번호를 사용했으니 검토 부탁드려요
 public enum BootLayer
 {
     DataManager = 2,
--- Assets/01.Script/00.Enum,Interface,Structer/InterfaceDeclare.cs
+++ Assets/01.Script/00.Enum,Interface,Structer/InterfaceDeclare.cs
@@ -8,6 +8,15 @@ public interface IBootStrapper
     /// 폴더 번호에 해당하는 BootLayer값 반환해주세요. public int BootOrder => (int)BootLayer.초기화필요한매니저;
     /// </summary>
     int BootOrder { get; }
+
+    /// <summary>
+    /// 필요한 다른 파사드를 context에서 받아 필드에 저장만 합니다.
+    /// </summary>
+    void IBootStrapperInject(BootstrapContext context);
+
+    /// <summary>
+    /// 초기화
+    /// </summary>
     void IBootStrapperInitialize(); //인자는 적당하게 구조체 만들어서 선언해주세요.
 
 }
