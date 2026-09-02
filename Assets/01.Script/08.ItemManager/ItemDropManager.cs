8db67e67ec66cb155cd2fba0a79a7f3a97fefbf3
FlynnSohn <137607802+FlynnSohn@users.noreply.github.com>
2026-09-02 10:55:24 +0900

    feat: Setting ItemDropManager

--- Assets/01.Script/08.ItemManager/ItemDropManager.cs
+++ Assets/01.Script/08.ItemManager/ItemDropManager.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class ItemDropManager : MonoBehaviour
+public class ItemDropManager : MonoBehaviour, IBootStrapper
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public int BootOrder => (int)BootLayer.ItemManager;
+
+    private readonly ItemDropProvider provider = new ItemDropProvider();
+
+
+    public void IBootStrapperInject(BootstrapContext context)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void IBootStrapperInitialize()
     {
-        
+
     }
+
+    public void RequestDrop(int dropTableId, Vector3 position)
+    {
+
+    }
+
+    // 프레임마다 몬스터 사망 처리 후 처리
+    private void LateUpdate()
+    {
+        PickUpAll();
+    }
+
+
+    private void PickUpAll()
+    {
+
+    }
+
+
 }
